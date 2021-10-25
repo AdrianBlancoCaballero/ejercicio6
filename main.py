dd955fc1a440f23f414fc16169e7a6ca86b34431
Adrián Blanco <adrianblancocaballero@gmail.com>
2021-10-25 22:40:06 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+# Ejercicio 6
+
+import statistics
+
+n1 = int(input("Introduzca el primer dato numérico: "))
+n2 = int(input("Introduzca el segundo dato numérico: "))
+n3 = int(input("Introduzca el tercer dato numérico: "))
+
+n = [n1, n2, n3]
+
+media = statistics.mean(n)
+mediana = statistics.median(n)
+varianza = statistics.variance(n)
+
+print("Media: ", media)
+print("Mediana: ", mediana)
+print("Varianza: ", varianza)
